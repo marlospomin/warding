3aa8c44eef96b963ee88938a2d7bb2a5f88dd301
Marlos Pomin <marlospomin@gmail.com>
2020-09-06 22:48:09 -0300

    Update install script

--- debug/install.sh
+++ debug/install.sh
@@ -3,9 +3,7 @@
 pacman -R man-pages --noconfirm
 pacman -S ruby glibc libxcrypt --noconfirm
 gem install bundle rake
-git clone https://github.com/marlospomin/warding
 export PATH="`ruby -e 'puts Gem.user_dir'`/bin:$PATH"
-cd warding
 bundle
 rake install
 warding
