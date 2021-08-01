733bcfd82ef88d46dc11ddd004029882ef33870d
Marlos Pomin <marlospomin@gmail.com>
2021-08-01 16:21:45 -0300

    Update yay install

--- lib/warding.rb
+++ lib/warding.rb
@@ -206,10 +206,10 @@ module Warding
           `arch-chroot /mnt pacman -S alsa-utils alsa-plugins alsa-lib --noc`
           # update package list
           `arch-chroot /mnt pacman -Syy`
-          # user creation --fix
+          # user creation
           `arch-chroot /mnt useradd -m -g wheel -s /bin/zsh ward`
           `arch-chroot /mnt sed -i 's/^#\s*\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' /etc/sudoers`
-          `arch-chroot /mnt sudo -u ward git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si; yay -S yay`
+          `arch-chroot /mnt sudo -u ward sh -c "git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si; yay -S yay"`
           `arch-chroot /mnt sudo -u ward sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended`
           # check if on VM
           if `arch-chroot /mnt dmidecode -s system-manufacturer`.include?("VMware, Inc.")
